4f5dcd9dfea98a496793e5cd965d99d9a6f31626
cicero2446daniel-wq <cicero2446daniel@gmail.com>
2026-03-31 19:30:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = "celula-v5-cache";
+const CACHE_NAME = "celulapro-v6-atualizado"; // Mudei o nome aqui para forçar o celular
 const assets = ["index.html", "manifest.json"];
 
 self.addEventListener("install", installEvent => {
+  self.skipWaiting(); // Força a nova versão a assumir o controle na hora
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Deleta o lixo da versão anterior
+          }
+        })
+      );
     })
   );
 });
