28d1fe60e100447eece8f27c8df9f22b684c9a80
cicero2446daniel-wq <cicero2446daniel@gmail.com>
2026-03-31 18:14:41 -0300

    Versão 5.0 com Gemini

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "celula-v5-cache";
+const assets = ["index.html", "manifest.json"];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
